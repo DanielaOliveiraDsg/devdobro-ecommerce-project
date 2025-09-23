58c97d47da05e746edbd6bd4b22081a04851fcc0
DanielaOliveiraDsg <danielaoliveiradsg@gmail.com>
2025-09-23 08:51:45 -0700

    open and close cart modal

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+//Objetivo: permitir que o usuário abra e feche o carrinho (modal)
+
+//inicialização do micromodal
+//faz um setup inicial
+document.addEventListener('DOMContentLoaded', () => {
+    MicroModal.init();
+});
